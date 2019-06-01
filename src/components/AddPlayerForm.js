ac051a1cfebd10aed7ca2866937207f9c75e14a5
KIM HAE YEON <devhaeyeon@naver.com>
2019-06-01 11:13:47 +0900

    ref 사용으로 uncontrolled로 component변환

--- src/components/AddPlayerForm.js
+++ src/components/AddPlayerForm.js
@@ -1,26 +1,20 @@
 import React from "react";
 export class AddPlayerForm extends React.Component {
-  state = {
-    value: ""
-  };
+  textInput = React.createRef();
 
-  constructor(props) {
-    super(props);
-  }
-
-  handleChange = event => {
-    this.setState({ value: event.target.value });
-  };
   // controled : vaildation할 때 실시간으로 보여w줌. vaildation이 필요 없으면 할 필요없다.
   // uncontroled component.
-  // 변경될 때마다 확인  controled component
+  // 변경될 때마다 확인 controled component
+  // core, dom, bom
   handleSubmit = event => {
     // 기본이벤트 새로고침 막기
     event.preventDefault();
     //event.stopPropagation();
     // event 버블링 막기 .
-    if (!this.state.value) return false;
-    this.props.addPlayer(this.state.value);
+    if (!this.textInput.current.value) return false;
+    // this.textInput.current === document.getElementById('id')
+    this.props.addPlayer(this.textInput.current.value);
+    event.currentTarget.reset();
   };
   render() {
     return (
@@ -28,7 +22,7 @@ export class AddPlayerForm extends React.Component {
         <input
           type="text"
           placeholder="enter a player's name"
-          value={this.state.value}
+          ref={this.textInput}
           onChange={this.handleChange}
         />
         <input type="submit" value="Add Player" />
